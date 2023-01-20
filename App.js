a21014bd22c0a5f01a60fbf2bef5876fa3feb601
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2023-01-20 09:13:28 -0300

    Acessando a biblioteca de midia

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Button, Image, View, Platform } from "react-native";
+import { useState } from "react";
+import { Button, Image, View } from "react-native";
+
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImagePickerExample() {
@@ -10,8 +11,8 @@ export default function ImagePickerExample() {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
+      aspect: [9, 16],
+      quality: 0.5,
     });
 
     console.log(result);
